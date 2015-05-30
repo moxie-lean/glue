5f5653332e33f46a3b5fd3f80346a591c5407cea
Crisoforo Gaspar Hernandez <mitogh@gmail.com>
2015-05-30 13:52:15 -0500

    Define comments for the plugin

---
+++ wp-content/plugins/glue/glue.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ *	Plugin Name: Glue
+ *	Plugin URI: https://github.com/mitogh/glue
+ *	Description: Provides glue
+ *	Version: 1.0
+ *	Author: Crisoforo Gaspar Hernandez
+ *	Author URI: https://github.com/mitogh
+ */
