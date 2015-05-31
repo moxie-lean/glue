d4cd3ea1a0e100182929043e8f2f4b654b48b98a
Crisoforo Gaspar Hernandez <mitogh@gmail.com>
2015-05-30 22:57:31 -0500

    Improve readability in view class and add comments

--- wp-content/plugins/glue/class-view.php
+++ wp-content/plugins/glue/class-view.php
@@ -1,14 +1,30 @@
 <?php namespace glue;
 
+/**
+ * Class to generate the view
+ */
 class View {
+  // Array with the data to pass to the view
   private $data;
+  // Variable to hold the instance of the FileHandle object
   private $file_handle;
 
+  /**
+   * Constructor of the object and FileHandle.
+   *
+   * @param   String  Path to the file
+   */
   public function __construct( $filename = '' ) {
     $this->data = array();
     $this->file_handle = new FileHandle( $filename );
   }
 
+  /**
+   * Static method to create the View object
+   *
+   * @param   String    Path to the file
+   * @return  View      Return the current object (View);
+   */
   public static function make( $filename = '' ) {
     $view = new View( $filename );
     // Process the file
@@ -16,10 +32,22 @@ class View {
     return $view;
   }
 
+  /**
+   * Another sintax to pass variables to the view. Refer to the next with method.
+   *
+   * @param   String    $key    They key to set.
+   * @param   mixed     $value  The value to store in $key.
+   */
   public function set( $key = '', $value = '' ) {
     $this->with( $key, $value );
   }
 
+  /**
+   * Method to store data and pass it to the PHP view where the data is displayed.
+   *
+   * @param   String    $key    They key to set.
+   * @param   mixed     $value  The value to store in $key.
+   */
   public function with( $key = '', $value = '' ) {
     if ( $key ) {
       $this->data[$key] = $value;
@@ -27,19 +55,55 @@ class View {
     return $this;
   }
 
+  /**
+   * Magic method to allows user retrive the data with the sintax $this->data like:
+   * $view->title, outpus the $title withoth using echo;
+   *
+   */
   public function __get( $key = '' ){
-    if ( $key && array_key_exists( $key, $this->data ) ) {
-      return $this->data[ $key ];
+    if ( $this->exist( $key ) ) {
+      echo $this->data[ $key ];
     }
   }
 
+  public function get( $key = '' ) {
+    if( $this->exist( $key ) ) {
+      return $this->data[ $key ];
+    } else {
+      return false;
+    }
+  }
+
+  /**
+   * Test if $key exist in the array of data
+   *
+   * @param   String    string name of the key to be tested
+   * @return  boolean   True if the $data array has the $key.
+   */
+  private function exist( $key = '' ) {
+    return $key && is_string( $key ) && array_key_exists( $key, $this->data );
+  }
+
+  /**
+   * Render the view and creates a list of variables based on the values pased
+   * to the view with the methods set and with, also creates a $view variable to
+   * better readability instead of $this, after that loads the file.
+   */
   public function render(){
     extract( $this->data );
+    $view = $this;
     include $this->file_handle->get_fullpath();
   }
 
-  public function exist( $variable ){
-    var_dump( isset( $variable ) );
+  /**
+   * Check if $variable exist and if has content
+   *
+   * @param   mixed     Variable to test, can be a string, array or boolean
+   * @return  boolean   Return true if $variable exist and it's not false,
+   *                    empty string, empty array, or zero.
+   */
+  public function has( $variable = '' ){
+    return isset( $variable ) && $variable;
   }
 }
 
