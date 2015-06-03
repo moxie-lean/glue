6760d0557ff84bb75489d703473cdea2abcccc1d
Crisoforo Gaspar Hernandez <mitogh@gmail.com>
2015-06-02 22:58:26 -0500

    Add loader to use un HTML pages outside theme

---
+++ loader.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Use this file to load the functions of WP and to have access to this class
+ * so for example:
+ *
+ * include '../../../plugins/glue/loader.php';
+ *
+ * and with that you have access to all the functions and View class.
+ */
+define('WP_USE_THEMES', false);
+require('../../../../wp-blog-header.php');
